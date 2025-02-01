4efdfc5429fa4616b2a8c14ebcd3a662f77e19d4
zelkalai <zelkalai@c4r7p10.1337.ma>
2025-02-01 10:05:16 +0100

    fk_left

--- include/cub3D.h
+++ include/cub3D.h
@@ -20,6 +20,8 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <structs_helper.h>
+# include <sys/time.h>
+# include <time.h>
 # include <unistd.h>
 
 # define SCREEN_HEIGHT 480
@@ -27,10 +29,10 @@
 # define MINIMAP_SCALE 0.3
 # define NUM_RAYS SCREEN_WIDTH
 # define FOV 1.0471975512
-# define OFFSET 5
-# define MAX_KEYS 6
-# define MOVE_SPEED 0.03
-# define ROTATION_SPEED 0.03
+# define OFFSET 4
+# define MAX_KEYS 7
+# define MOVE_SPEED 0.1
+# define ROTATION_SPEED 0.1
 # define PITCH_FACTOR 1
 
 typedef struct s_pl
@@ -51,6 +53,21 @@ typedef struct s_img
 	int				height;
 }					t_img;
 
+typedef struct s_spirit
+{
+	t_img			aim[21];
+	int				animating;
+	int				current_frame;
+	unsigned long	last_frame;
+
+}					t_spirit;
+
+typedef struct s_door
+{
+	int				x;
+	int				y;
+}					t_door;
+
 typedef struct s_data
 {
 	struct s_mlx	*mlx;
@@ -70,6 +87,11 @@ typedef struct s_data
 	t_img			tex_so;
 	t_img			tex_ea;
 	t_img			tex_we;
+	int				mouse_moving;
+	int				last_mouse_x;
+	unsigned long	last_mouse_time;
+	t_spirit		spirit;
+	t_img			door;
 }					t_data;
 
 typedef struct s_mlx
@@ -82,6 +104,7 @@ typedef struct s_mlx
 }					t_mlx;
 
 /*main.c*/
+int					red_cross(t_mlx *mlx);
 void				ray_casting(t_mlx *mlx, t_data *data);
 int					game_loop(t_mlx *mlx);
 int					init(t_mlx *mlx, t_data *data, char *argv, t_pl *player);
@@ -103,15 +126,25 @@ int					set_map(char *file, t_data *data);
 int					check_file(char *file);
 int					parse(char *file, t_data *data);
 
+/*mouse.c*/
+int					mouse_rotation(int x, int y, t_data *data);
+int					mouse_click(int keycode, int x, int y, t_data *data);
+void				wait_for_frame(t_data *data);
+void				check_frame(t_mlx *mlx);
+void				left_click(t_mlx *mlx);
+
 /*keys.c*/
+void				check_door(t_data *data);
 void				key_mouvment(t_mlx *mlx);
 int					key_rotation(t_mlx *mlx);
 int					key_press(int keycode, t_data *data);
 int					key_release(int keycode, t_data *data);
-int					red_cross(t_mlx *mlx);
 
 /*keys_helper.c*/
+unsigned long		get_current_time(void);
 void				get_direction_vector(double angle, double *dx, double *dy);
+int					check_corner(t_data *data, t_mlx *mlx, double new_x,
+						double new_y);
 void				collision_detection(t_mlx *mlx, double *new_x,
 						double *new_y);
 void				change_x_y(t_mlx *mlx, t_key_mouvment *k);
--- src/free.c
+++ src/free.c
@@ -27,6 +27,18 @@ void	free_map(t_data *data)
 
 void	clean_all(t_mlx *mlx)
 {
+	int	i;
+
+	mlx_destroy_image(mlx->mlx, mlx->data->tex_no.mlx_image);
+	mlx_destroy_image(mlx->mlx, mlx->data->tex_so.mlx_image);
+	mlx_destroy_image(mlx->mlx, mlx->data->tex_we.mlx_image);
+	mlx_destroy_image(mlx->mlx, mlx->data->tex_ea.mlx_image);
+	mlx_destroy_image(mlx->mlx, mlx->data->door.mlx_image);
+	i = -1;
+	while (++i < 21)
+		mlx_destroy_image(mlx->mlx, mlx->data->spirit.aim[i].mlx_image);
+	mlx_clear_window(mlx->mlx, mlx->win);
+	mlx_destroy_image(mlx->mlx, mlx->img.mlx_image);
 	mlx_destroy_window(mlx->mlx, mlx->win);
 	mlx_destroy_display(mlx->mlx);
 	free_map(mlx->data);
